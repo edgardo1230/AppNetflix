466b8de568b48c62fe728c64c3ef9c044f6a9471
edgardo1230 <edgardo1230@gmail.com>
2017-12-28 23:41:34 -0600

    Carga Inicial

---
+++ src/main/java/com/raziem/netflix/AppNetflixApplication.java
@@ -0,0 +1,12 @@
+package com.raziem.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNetflixApplication.class, args);
+	}
+}
